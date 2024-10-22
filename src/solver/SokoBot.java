f95d0773654ff8879c6a4059a7e3c0662f89224f
Mariella Jeanne <150491048+mariellajeanne@users.noreply.github.com>
2024-10-22 14:52:26 +0800

    Implemented Search.

--- src/solver/SokoBot.java
+++ src/solver/SokoBot.java
@@ -7,17 +7,9 @@ public class SokoBot {
     // javac -d out -cp "src/solver/commons-lang3-3.17.0.jar;out" src/gui/*.java src/main/*.java src/reader/*.java src/solver/*.java
     // java -cp "out;src/solver/commons-lang3-3.17.0.jar" main.Driver
 
-    /*
-     * Default stupid behavior: Think (sleep) for 3 seconds, and then return a
-     * sequence
-     * that just moves left and right repeatedly.
-     */
-    try {
-      Thread.sleep(3000);
-    } catch (Exception ex) {
-      ex.printStackTrace();
-    }
-    return "lrlrlrlrlrlrlrlrlrlrlrlrlrlrlrlrlrlrlrlrlrlrlrlrlrlrlrlrlrlrlrlrlrlrlrlrlrlr";
+    State state = new State(mapData, itemsData);
+    Search search = Search.getInstance();
+    return search.getSequence(state);
   }
 
 }
