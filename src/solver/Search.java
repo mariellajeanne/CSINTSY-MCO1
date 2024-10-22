9fad0d0c28483c4bdecc17d313d115c71fb7b054
Mariella Jeanne <150491048+mariellajeanne@users.noreply.github.com>
2024-10-22 14:51:58 +0800

    Used String hash sets.

--- src/solver/Search.java
+++ src/solver/Search.java
@@ -12,104 +12,116 @@ import java.util.*;
 */
 public class Search
 {
-    // // The single instance of the search class.
-    // private static Search search;
+    // The single instance of the search class.
+    private static Search search;
 
-    // // The single instance of the status class.
-    // private static Status status;
+    // The set of visited playerCoor and boxCoor states.
+    private HashSet<String> visitedStates = new HashSet<>();
 
-    // // The set of visited box coor states.
-    // private HashSet<HashSet<int[]>> visitedStates = new HashSet<>();
+    // The queue of states.
+    private Queue<State> queue = new ArrayDeque<>();
 
-    // private Queue<State> queue = new ArrayDeque<>();
+    // The player's moves and their corresponding offsets.
+    private final char[] moves = {'l', 'r', 'u', 'd'};
+    private final int[] offsetx = {-1, 1, 0, 0};
+    private final int[] offsety = {0, 0, -1, 1};
 
-    // private final char[] moves = {'l', 'r', 'u', 'd'};
-    // private final int[] offsetx = {-1, 1, 0, 0};
-    // private final int[] offsety = {0, 0, -1, 1};
+    /**
+     * Returns the single instance of the class.
+     * 
+     * @return {Search}
+     */
+    public static Search getInstance()
+    {
+        if (search == null)
+            search = new Search();
+        return search;
+    }
 
-    // /**
-    //  * Constructs the single search instance.
-    //  */
-    // private Search()
-    // {
-    //     status = Status.getInstance();
-    // }
+    /**
+     * Returns the sequence of the goal state's moves.
+     * Returns "" otherwise.
+     * 
+     * @param startingState {State} the starting state
+     * 
+     * @return {String}
+     */
+    public String getSequence(State startingState)
+    {
+        queue.add(startingState);
+        // startingState.visit();
+        addToVisitedStates(startingState);
 
-    // /**
-    //  * Returns the single instance of the class.
-    //  * 
-    //  * @return {Search}
-    //  */
-    // public static Search getInstance()
-    // {
-    //     if (search == null)
-    //         search = new Search();
-    //     return search;
-    // }
+        while (!queue.isEmpty())
+        {
+            State currState = queue.poll();
 
-    // /**
-    //  * Returns the sequence of the goal state's moves.
-    //  * Returns "" otherwise.
-    //  * 
-    //  * @param startingState {State} the starting state
-    //  * 
-    //  * @return {String}
-    //  */
-    // public String getSequence(State startingState)
-    // {
-    //     queue.add(startingState);
-    //     // startingState.visit();
-    //     visitedStates.add(startingState.boxCoor);
-
-    //     while (!queue.isEmpty())
-    //     {
-    //         State currState = queue.poll();
-
-    //         if (status.isWin(currState))
-    //         {
-    //             return reversePath(currState);
-    //         }
+            if (currState.boxCoor.equals(State.targetCoor))
+            {
+                return reversePath(currState);
+            }
 
 
-    //         // go through each of the next states (left, right, up, down):
-    //         for (int i = 0; i < 4; i++) {
-    //             State nextState = currState.movePlayer(moves[i], offsetx[i], offsety[i]);
-    //             if (nextState == null)
-    //             {
-    //                 continue;
-    //             }
-    //             if (nextState.boxCoor.equals(State.targetCoor))
-    //             {
-    //                 return reversePath(nextState);
-    //             }
-    //             if (!visitedStates.contains(nextState.boxCoor))
-    //             {
-    //                 queue.add(nextState);
-    //                 visitedStates.add(nextState.boxCoor);
-    //             }
-    //         }
-    //     }
+            // go through each of the next states (left, right, up, down):
+            for (int i = 0; i < 4; i++) {
+                State nextState = currState.movePlayer(moves[i], offsetx[i], offsety[i]);
+                if (nextState == null)
+                {
+                    continue;
+                }
+                if (nextState.boxCoor.equals(State.targetCoor))
+                {
+                    return reversePath(nextState);
+                }
+                if (!isVisited(nextState))
+                {
+                    queue.add(nextState);
+                    addToVisitedStates(nextState);
+                }
+            }
+        }
 
-    //     return "";
-    // }
+        return "";
+    }
 
-    // /**
-    //  * Reconstructs the path taken from the given end state to the starting state.
-    //  * 
-    //  * @param endState {State} the end state
-    //  * @return {String}
-    //  */
-    // private String reversePath(State endState)
-    // {
-    //     StringBuilder path = new StringBuilder(endState.prevMove);
+    /**
+     * Reconstructs the path taken from the given end state to the starting state.
+     * 
+     * @param endState {State} the end state
+     * @return {String}
+     */
+    private String reversePath(State endState)
+    {
+        StringBuilder path = new StringBuilder(endState.prevMove);
 
-    //     State currState = endState.getPrevState();
-    //     while (currState != null)
-    //     {
-    //         path.append(currState.prevMove);
-    //         currState = currState.prevMove;
-    //     }
+        State currState = endState.prevState;
+        while (currState != null)
+        {
+            path.append(currState.prevMove);
+            currState = currState.prevState;
+        }
 
-    //     return path.reverse().toString();
-    // }
+        return path.reverse().toString();
+    }
+
+    /**
+     * Adds the state to the visited states.
+     * 
+     * @param state {State} The state to add.
+     */
+    private void addToVisitedStates(State state)
+    {
+        visitedStates.add(state.getHashCode());
+    }
+
+    /**
+     * Checks if the state has already been visited.
+     * 
+     * @param state {State} The state to check.
+     * @return {boolean}
+     */
+    private boolean isVisited(State state)
+    {
+        return visitedStates.contains(state.getHashCode());
+    }
 }
