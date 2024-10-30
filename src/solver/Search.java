2babfda5dd4fa2f3f0ed41aa85b8961020618392
Mariella Jeanne <150491048+mariellajeanne@users.noreply.github.com>
2024-10-31 00:33:13 +0800

    Optimized computing time.

--- src/solver/Search.java
+++ src/solver/Search.java
@@ -9,107 +9,113 @@ import java.util.*;
 
 /**
  * The search class.
-*/
+ */
 public class Search
 {
-    // // The single instance of the search class.
-    // private static Search search;
+    // The single instance of the search class.
+    private static Search search;
 
-    // // The single instance of the status class.
-    // private static Status status;
+    // The set of the visited states' hash codes.
+    private final HashSet<String> visitedStates = new HashSet<>();
 
-    // // The set of visited box coor states.
-    // private HashSet<HashSet<int[]>> visitedStates = new HashSet<>();
+    // The queue of states to be visited.
+    private final ArrayDeque<State> queue = new ArrayDeque<>();
 
-    // private Queue<State> queue = new ArrayDeque<>();
+    // The player's moves and their corresponding offsets.
+    public static final char[] moves = {'u', 'r', 'd', 'l'};
+    public static final int[] xOffset = {0, 1, 0, -1};
+    public static final int[] yOffset = {-1, 0, 1, 0};
 
-    // private final char[] moves = {'l', 'r', 'u', 'd'};
-    // private final int[] offsetx = {-1, 1, 0, 0};
-    // private final int[] offsety = {0, 0, -1, 1};
+    /**
+     * Returns the single instance of the class.
+     * 
+     * @return {Search}
+     */
+    public static Search getInstance()
+    {
+        if (search == null)
+            search = new Search();
+        return search;
+    }
 
-    // /**
-    //  * Constructs the single search instance.
-    //  */
-    // private Search()
-    // {
-    //     status = Status.getInstance();
-    // }
+    /**
+     * Returns the sequence of the goal state's moves using Breadth-First Search.
+     * Returns "" otherwise.
+     * 
+     * @param startingState {State} The starting state.
+     * 
+     * @return {String}
+     */
+    public String getSequenceBFS(State startingState)
+    {
+        queue.add(startingState);
+        visitedStates.add(startingState.getHashCode());
 
-    // /**
-    //  * Returns the single instance of the class.
-    //  * 
-    //  * @return {Search}
-    //  */
-    // public static Search getInstance()
-    // {
-    //     if (search == null)
-    //         search = new Search();
-    //     return search;
-    // }
+        while (!queue.isEmpty())
+        {
+            // Dequeues the head of the queue.
+            State currState = queue.poll();
 
-    // /**
-    //  * Returns the sequence of the goal state's moves.
-    //  * Returns "" otherwise.
-    //  * 
-    //  * @param startingState {State} the starting state
-    //  * 
-    //  * @return {String}
-    //  */
-    // public String getSequence(State startingState)
-    // {
-    //     queue.add(startingState);
-    //     // startingState.visit();
-    //     visitedStates.add(startingState.boxCoor);
+            // Checks each move from the current state (i.e., left, right, up, and down).
+            for (int i = 0; i < 4; i++)
+            {
+                // Gets the next state transitioned from the move.
+                State nextState;
+                try
+                {
+                    nextState = State.movePlayer(new State(currState), moves[i], xOffset, yOffset, i);
+                }
+                catch (Exception e)
+                {
+                    continue;
+                }
+                
+                // Checks the next state if it isn't null.
+                if (nextState != null)
+                {
+                    // Gets the hash code of the state.
+                    String code = nextState.getHashCode();
 
-    //     while (!queue.isEmpty())
-    //     {
-    //         State currState = queue.poll();
+                    // Adds the next state to the queue if it has not yet been visited.
+                    if (!visitedStates.contains(code))
+                    {
+                        queue.add(nextState);
+                        visitedStates.add(code);
+                    }
 
-    //         if (status.isWin(currState))
-    //         {
-    //             return reversePath(currState);
-    //         }
+                    // Returns the path to the last state.
+                    if (nextState.boxCoor.equals(State.targetCoor) || queue.isEmpty())
+                        return reversePath(nextState);
+                }    
+            }
+        }
+        
+        return "";
+    }
 
+    /**
+     * Reconstructs the path taken from the given end state to the starting state.
+     * 
+     * @param endState {State} The end state.
+     * 
+     * @return {String}
+     */
+    private String reversePath(State endState)
+    {   
+        // Starts building the path with the last move.
+        StringBuilder path = new StringBuilder().append(endState.prev.move);
 
-    //         // go through each of the next states (left, right, up, down):
-    //         for (int i = 0; i < 4; i++) {
-    //             State nextState = currState.movePlayer(moves[i], offsetx[i], offsety[i]);
-    //             if (nextState == null)
-    //             {
-    //                 continue;
-    //             }
-    //             if (nextState.boxCoor.equals(State.targetCoor))
-    //             {
-    //                 return reversePath(nextState);
-    //             }
-    //             if (!visitedStates.contains(nextState.boxCoor))
-    //             {
-    //                 queue.add(nextState);
-    //                 visitedStates.add(nextState.boxCoor);
-    //             }
-    //         }
-    //     }
+        // Gets the details of the state previous to the end state.
+        Prev prevDetails = endState.prev.prevDetails;
 
-    //     return "";
-    // }
+        // Constructs the path in reverse.
+        while (prevDetails != null)
+        {
+            path.append(prevDetails.move);
+            prevDetails = prevDetails.prevDetails;
+        }
 
-    // /**
-    //  * Reconstructs the path taken from the given end state to the starting state.
-    //  * 
-    //  * @param endState {State} the end state
-    //  * @return {String}
-    //  */
-    // private String reversePath(State endState)
-    // {
-    //     StringBuilder path = new StringBuilder(endState.prevMove);
-
-    //     State currState = endState.getPrevState();
-    //     while (currState != null)
-    //     {
-    //         path.append(currState.prevMove);
-    //         currState = currState.prevMove;
-    //     }
-
-    //     return path.reverse().toString();
-    // }
+        // Returns the reversed constructed path.
+        return path.reverse().toString();
+    }
 }
